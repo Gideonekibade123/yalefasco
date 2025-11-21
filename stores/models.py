2c75da84881b15ea85e83f0b87e164c4f7be483e
Gideon <ekibade.gideon@yahoo.comm>
2025-11-21 12:08:23 +0100

    the new changes

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class category(models.Model):
+    title=models.CharField(max_length=225)
+    image=models.ImageField(upload_to='category', null=True, blank=True)
+    created_at=models.DateTimeField(auto_now_add=False)
+
+    #collection
+class collection(models.Model):
+    title=models.CharField(max_length=225)
